f8c8dedbf86928e57d65317ed897812dfc274f56
javiortig <dev.javiog@gmail.com>
2023-05-10 19:37:47 +0200

    TODO

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express")
 const cors = require("cors")
 const morganBody = require("morgan-body")
-//const swaggerUi = require("swagger-ui-express")
+const swaggerUi = require("swagger-ui-express")
 require("dotenv").config();
-//const swaggerSpecs = require("./docs/swagger")
+const swaggerSpecs = require("./docs/swagger")
 const loggerStream = require("./utils/handleLogger")
 const { sequelize, dbConnectMySql } = require("./config/mysql")
+
+/*
+    LISTA DE TODO POR ORDEN DE IMPORTANCIA:
+    - Dejar Swagger funcionando. (mirar apuntes).
+    - Pruebas automaticas con Jest.
+    - Probar que las imagenes y tal funcionen, tanto al subirlas, actualizar... como al get de las paginas que devuelva las urls
+    - Añadir que ordene las webpages por score
+
+    - En consecuencia al punto 3, añadir un endpoint para borrar imagenes
+*/
 
 // Esto se borra luego, es para testear. Se deberá incluir en los controllers
 const { interestsModel } = require('./models/')
@@ -19,8 +29,8 @@ const app = express()
 app.use(cors()) 
 app.use(express.json())
 
-//Le digo que directorio es publico
-app.use(express.static("storage")) // http://localhost:3000/file.jpg
+// //Le digo que directorio es publico
+// app.use(express.static("storage")) // http://localhost:3000/file.jpg
 
 // 1.- Sniffer de todo las peticiones y respuestas
 morganBody(app, { //Para ver las distintas configuraciones que podemos pasarle en este objeto, mirad la doc en la parte de API
